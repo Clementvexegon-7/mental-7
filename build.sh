aa48a15c597b4ab5e55f77db8426172fe6e1d405
Clementvexegon-7 <clementvexegon@gmail.com>
2026-03-22 20:45:59 +0300

    Ready for deployment

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install gunicorn django whitenoise dj-database-url psycopg2-binary Pillow
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+chmod +x build.sh
